60ad79f8c45aa88e6ec04aee12719f738538a3a4
Harry Day <contact@harryday.xyz>
2023-12-03 13:30:39 +0000

    Add day 3 beginning

---
+++ Day-03/Day-03.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	content, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		fmt.Println("An error occurred while reading the input. Exiting...")
+		return
+	}
+
+	games := strings.Split(string(content), "\n")
+
+	//println("Part 1:", part1(games))
+}
---
+++ Day-03/go.mod
@@ -0,0 +1,3 @@
+module Day-03
+
+go 1.21.3
