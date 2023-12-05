fd2e2a62391afe92cb5539090757a69a6f3c1ec6
Harry Day <contact@harryday.xyz>
2023-12-05 07:34:52 +0000

    First attempt at part1

--- Day-05/part1.go
+++ Day-05/part1.go
@@ -1,9 +1,40 @@
 package main
 
-import "fmt"
-
 func part1(lines []string) int {
 	var p = cleanData(lines)
-	fmt.Println(p)
-	return 0
+	var lowestLocation = int(^uint(0) >> 1)
+
+	for seed, _ := range p.seeds {
+		soil, prs := p.seedToSoil[seed]
+		if !prs {
+			soil = seed
+		}
+		fertiliser, prs := p.soilToFertiliser[soil]
+		if !prs {
+			fertiliser = soil
+		}
+		water, prs := p.fertiliserToWater[fertiliser]
+		if !prs {
+			water = fertiliser
+		}
+		light, prs := p.waterToLight[water]
+		if !prs {
+			light = water
+		}
+		temperature, prs := p.lightToTemperature[light]
+		if !prs {
+			temperature = light
+		}
+		humidity, prs := p.temperatureToHumidity[temperature]
+		if !prs {
+			humidity = temperature
+		}
+		location, prs := p.humidityToLocation[humidity]
+		if !prs {
+			location = humidity
+		}
+		lowestLocation = min(lowestLocation, location)
+	}
+
+	return lowestLocation
 }
